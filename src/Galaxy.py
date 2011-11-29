061b46a35d901c3f36d8003c3bc310c1fda52cc4
MJ-meo-dmt <francois.debruyn66@gmail.com>
2011-11-29 16:27:49 +0200

    Added testing planets, problems with lights

--- src/Galaxy.py
+++ src/Galaxy.py
@@ -110,6 +110,7 @@ class Galaxy(DirectObject):
         
         # Init Lights
         spaceLightsInit = SpaceLights()
+        #spaceLightsInit.lightSetup()
         self.skyBox.setLightOff()
         
         # Init Planet Creation
@@ -124,28 +125,37 @@ class Galaxy(DirectObject):
 
 # SpaceLights
 # Everything todo with lights in space.#
-class SpaceLights:
+class SpaceLights():
     
     def __init__(self):
+        pass
+        # Create Light
+        #----------------------------------------------------------------------------------->
+    def lightSetup():
         
-        #render.setAttrib(LightRampAttrib.makeHdr1())
+        planetInit = Planets()
+        sun1 = planetInit.earthPlanet.sun1()
+        
+        # Setup Ambient light
         ambientLight = AmbientLight("ambientLight")
-        # existing lighting is effectively darkened so boost ambient a bit
-        ambientLight.setColor(Vec4(.4, .4, .4, 1))
-        
-        directionalLight = DirectionalLight("directionalLight")
-        directionalLight.setDirection(Vec3(-5, -5, -5))
-        # hdr can handle any amount of lighting
-        # lets make things nice and sunny
-        directionalLight.setColor(Vec4(2.0, 2.0, 2.0, 1.0))
-        directionalLight.setSpecularColor(Vec4(2.0, 2.0, 2.0, 1))
-        
+        ambientLight.setColor(Vec4(0.1, 0.1, 0.1, 1))
         render.setLight(render.attachNewNode(ambientLight))
-        render.setLight(render.attachNewNode(directionalLight))
+
+        # Adding Sun light.
+        plight = PointLight('plight')
+        plight.setColor(VBase4(0.5, 0.5, 0.5, 1))
+        #plight.setAttenuation(Point3(0.9,0, 0))
+        sunLight = sun1.attachNewNode(plight)
+        self.sunLight.setPos(0,0,0)
+        render.setLight(sunLight)
+    
+
+#----------------------------------------------------------------------------------->
+
 # END OF SpaceLights CLASS
 
 # Planet control and creation.
-class Planets:
+class Planets(SpaceLights):
     
     def __init__(self):
         
@@ -159,28 +169,89 @@ class Planets:
         # Here is the code for the creation of the planets
         DB = dbMain()
         
-        
-        self.orbit_root_testPlanet2 = render.attachNewNode('orbit_root_testPlanet2')
-        
-        xyzSum = 2.6 
-        
-        self.testPlanet2 = loader.loadModel("../resources/models/p_Earth2.egg")
-        
-        self.testPlanet2.reparentTo(self.orbit_root_testPlanet2)
-        self.testPlanet2.setScale(xyzSum)
-        self.testPlanet2.setPos(0.48300,0.15400,0)
-        
+        #####
+        ## PLANET LOAD.
+        ##
+        ##  THIS IS ONLY FOR TESTING NO SQL DATA IS EVEN BEING USED.
         ###
         
-        self.orbit_root_testPlanet = render.attachNewNode('orbit_root_testPlanet')
+        # Setting up custom materials.
+        self.mat1 = Material()
+        self.mat1.setShininess(2.0)
+        self.mat1.setDiffuse(VBase4(1,1,0,1))
+        self.mat1.setAmbient(VBase4(1,1,0,1))
+        self.mat1.setEmission(VBase4(1,1,0,1))
+        self.mat1.setSpecular(VBase4(0.8,0.6,0,1))
+        self.mat1.setTwoside(True)
+
+
+
+        self.solarSystem1 = render.attachNewNode('solarSystem1')
+        self.sun1 = self.solarSystem1.attachNewNode('sun1')
+        self.planet1 = self.sun1.attachNewNode('planet1')
+        self.planet2 = self.sun1.attachNewNode('planet2')
+        self.planet3 = self.sun1.attachNewNode('planet3')
+        self.planet4 = self.sun1.attachNewNode('planet4')
+        self.planet5 = self.sun1.attachNewNode('planet5')
+        self.planet6 = self.sun1.attachNewNode('planet6')
+        self.planet7 = self.sun1.attachNewNode('planet7')
+        self.planet8 = self.sun1.attachNewNode('planet8')
+        self.planet9 = self.sun1.attachNewNode('planet9')
         
-        xyzSum2 = 0.0012 * self.galaxyScale
         
-        self.testPlanet = loader.loadModel("../resources/models/p_Earth2.egg")
+        dummySphere = "../resources/models/planet_sphere"
+
+        self.sun = loader.loadModel(dummySphere)
+        self.sun.setMaterial(self.mat1)
+        self.sun.reparentTo(self.sun1)
+        self.sun.setScale(3.0)
+        self.sun.setPos(0,0,0)
         
-        self.testPlanet.reparentTo(self.orbit_root_testPlanet2)
-        self.testPlanet.setScale(xyzSum2, xyzSum2, xyzSum2)
-        self.testPlanet.setPos(0.15000,0.15000,0)
+        self.mercury = loader.loadModel(dummySphere)
+        self.mercury.reparentTo(self.planet1)
+        self.mercury.setPos((0.076*30)+ 4.712,0,0)
+        self.mercury.setScale(0.01)
+
+        self.venus = loader.loadModel(dummySphere)
+        self.venus.reparentTo(self.planet2)
+        self.venus.setPos((0.142*30)+ 4.712,0,0)
+        self.venus.setScale(0.026)
+
+        self.earth = loader.loadModel(dummySphere)
+        self.earth.reparentTo(self.planet3)
+        self.earth.setPos((0.197*30)+4.712,0,0)
+        self.earth.setScale(0.027)
+
+        self.mars = loader.loadModel(dummySphere)
+        self.mars.reparentTo(self.planet4)
+        self.mars.setPos((0.300*30)+4.712,0,0)
+        self.mars.setScale(0.015)# 1.5cm
+
+        self.jupiter = loader.loadModel(dummySphere)
+        self.jupiter.reparentTo(self.planet5)
+        self.jupiter.setPos((1.025*30)+4.712,0,0)
+        self.jupiter.setScale(0.300)# 
+
+        self.saturn = loader.loadModel(dummySphere)
+        self.saturn.reparentTo(self.planet6)
+        self.saturn.setPos((1.880*30)+4.712,0,0)
+        self.saturn.setScale(0.250)
+
+        self.uranus = loader.loadModel(dummySphere)
+        self.uranus.reparentTo(self.planet7)
+        self.uranus.setPos((3.780*30)+4.712,0,0)
+        self.uranus.setScale(0.100)
+
+        self.neptune = loader.loadModel(dummySphere)
+        self.neptune.reparentTo(self.planet8)
+        self.neptune.setPos((5.920*30)+4.712,0,0)
+        self.neptune.setScale(0.098)
+
+        self.pluto = loader.loadModel(dummySphere)
+        self.pluto.reparentTo(self.planet9)
+        self.pluto.setPos((7.780*30)+4.712,0,0)
+        self.pluto.setScale(0.005)
+       
     
 ### END OF Planets CLASS
 
@@ -193,7 +264,7 @@ class Station:
         # test model 
         self.Sstasion = loader.loadModel("../resources/models/SpaceStation.egg")
         #self.sstasion.setScale(0,0,0)
-        self.Sstasion.setPos(0.14000,0.14000,0.0010)
+        self.Sstasion.setPos(0.14000,0.14000,0.0010) 
         self.Sstasion.reparentTo(render)
 # END OF SpaceStationControl CLASS.
 
